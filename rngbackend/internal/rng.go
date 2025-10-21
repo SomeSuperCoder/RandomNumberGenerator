fec0c167454d547b370d50209652e83791adc9f9
SomeSuperCoder <ithelper9@gmail.com>
2025-10-21 14:59:25 +0300

    Get block hashes from Solana

--- rngbackend/internal/rng.go
+++ rngbackend/internal/rng.go
@@ -1,7 +1,13 @@
 package internal
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
 	"math/rand"
+	"net/http"
 	"strconv"
 	"time"
 )
@@ -15,8 +21,62 @@ type Pipeline struct {
 	Result  float64    `json:"result"`
 }
 
+// Salana response structure
+type GetLatestBlockhashResponse struct {
+	JSONRPC string
+	Result  struct {
+		Value struct {
+			Blockhash string
+		}
+	}
+}
+
 const k = 1000
 const step = 5
+
+func GetSalanaHash() string {
+	var response GetLatestBlockhashResponse
+	data := map[string]interface{}{
+		"jsonrpc": "2.0",
+		"id":      1,
+		"method":  "getLatestBlockhash",
+		"params": []interface{}{
+			map[string]interface{}{
+				"commitment": "processedzed",
+			},
+		},
+	}
+
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		fmt.Println("Error marshaling JSON:", err)
+		return "1"
+	}
+
+	resp, err := http.Post(
+		"https://api.devnet.solana.com",
+		"application/json",
+		bytes.NewBuffer(jsonData),
+	)
+	if err != nil {
+		fmt.Println("Error making request:", err)
+		return "2"
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error reading response:", err)
+		return "3"
+	}
+	err = json.Unmarshal([]byte(string(body)), &response)
+
+	if err != nil {
+		log.Fatal("Error parsing JSON:", err)
+	}
+	return response.Result.Value.Blockhash
+
+}
 
 func Process(hashes []string) float64 {
 	return Result(
